62e05b5505a3a4e34ed45f1c7d57bfea0100379c
Guilherme Santiago <guilhermesantiagors@gmail.com>
2023-08-03 15:48:15 -0300

    Update index.php

--- combo/index.php
+++ combo/index.php
@@ -1,1 +1,218 @@
+<!DOCTYPE html>
+
+<html lang="pt-br" class="">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+    <title>ComboCX</title>
+
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="shortcut icon" href="images/icon.png" type="image/x-icon">
+
+</head>
+<body>
+    <section class="bg-gray pd-60">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+
+                    <img src="images/logo.png" class="logo" alt="Logo Claravista">
+
+                    <h2 class="orange">Maximize seus resultados encantando
+                        seus clientes  com a Zendesk</h2>
+                    <h3 class="desc-title mt-30 text-white">Turbine seus resultados com uma ferramenta flexível e personalizável, que atende as necessidades de sua empresa.</h3>
+            
+                    <a href="#form-demonstracao" title="Solicitar Demonstração" class="btn-orange mt-30 display">Solicitar Demonstração</a>
+                </div>
+                <div class="col">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container reset-mobile">
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <img src="images/hero-2.png" width="100%" alt="">
+            </div>
+            <div class="col-md-6 col-sm-12 pe-30">
+                <h2 class="orange mt-60">Potencializamos negócios com a tecnologia Zendesk</h2>
+                <p class="text-body">Nossa missão é criar pontes entre o atendimento da sua empresa e seus clientes, por meio de estratégias e processos. Utilizamos o melhor da tecnologia e da IA para conectar pessoas.</p>
+                <a href="#form-demonstracao" class="btn-orange db mt-60">SOLICITE UMA DEMONSTRAÇÃO</a>
+            </div>
+        </div>
+    </section>
+
+    <div class="bg-lgray pd-60">
+        <div class="container itens">
+            <div class="row">
+                <div class="col-md-6 col-sm-12 row">
+                    <div class="col-md-5 text-left">
+                        <h3 class="orange">Otimize seus<br>
+                        indicadores</h3>
+                    </div>
+                    <div class="col-md-7 text-left">
+                        <img width="60%" src="images/icon-01.png" alt="Crescimento de indicadores">
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12 row">
+                    <div class="col-md-4 text-center">
+                        <img src="images/icon-02.png" alt="Tempo de Atendimento">
+                    </div>
+                    <div class="col-md-8">
+                        <h3 class="black m0">Reduza seu tempo médio de atendimento</h3>
+                        <div class="desc mt-10">
+                            Aumente a produtividade e impulsione seus resultados. Com menos tempo de atendimento, você garante uma experiência do cliente aprimorada, redução de custos e fortalecimento da reputação da sua marca.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br><br>
+            <div class="row mt-30">
+                <div class="col-md-6 col-sm-12 row">
+                    <div class="col-md-4 text-left">
+                        <img src="images/icon-03.png" alt=" First Call Resolution">
+                    </div>
+                    <div class="col-md-8">
+                        <h3 class="black m0">Diminua seu First Call Resolution</h3>
+                        <div class="desc mt-10">
+                            Alcance resultados surpreendentes ao aumentar o FCR. Resolvendo os problemas dos clientes logo no primeiro contato, você proporciona uma experiência excepcional, gerando satisfação e fidelização. Além de reduzir os custos operacionais.
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12 row">
+                    <div class="col-md-4 text-center">
+                        <img src="images/icon-04.png" alt=" Deflexão de Tickets">
+                    </div>
+                    <div class="col-md-8">
+                        <h3 class="black m0">Deflexão de tickets</h3>
+                        <div class="desc mt-10">
+                            Com um bot inteligente, você oferece suporte instantâneo 24 horas por dia, 7 dias por semana, proporcionando conveniência aos seus clientes. Além disso, o bot agiliza o atendimento, resolvendo solicitações simples de forma rápida e eficiente.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br><br>
+            <div class="row text-center">
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <a href="#form-demonstracao" class="btn-orange db mt-60">SOLICITE UMA DEMONSTRAÇÃO</a>
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+    </div>
+
+    <section class="container pd-60">
+        <div class="row text-center">
+            <h2 class="orange">Nossos serviços</h2>
+        </div>
+
+        <div class="row mt-100">
+            <div class="col-md-4  text-center">
+                <div class="card bg-orange3">
+                    <img src="images/icon-05.png" alt="Consultoria">
+                    <h3>Consultoria</h3>
+                    <hr>
+                    <div class="desc">
+                        Temos consultores especializados e experientes para oferecer soluções que levem sua empresa um passo adiante em qualquer etapa da jornada em que você esteja.
+                    </div>
+                </div>
+                
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="card bg-orange2">
+                    <img src="images/icon-06.png" alt="Consultoria">
+                    <h3 class="">Implementação</h3>
+                    <hr>
+                    <div class="desc">
+                        Escolha a tecnologia certa para aumentar a satisfação de seus clientes.
+                    </div>
+                    
+                </div>
+                
+            </div>
+            <div class="col-md-4  text-center">
+                <div class="card bg-orange">
+                    <img src="images/icon-07.png" alt="Consultoria">
+                    <h3>Sustentação</h3>
+                    <hr>
+                    <div class="desc">
+                        Cresça sem dor! Obtenha um suporte descomplicado e contínuo para estar em uma evolução constante do seu negócio.
+                    </div>
+                    
+                </div>
+                
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="container text-center pd-60">
+        <div class="row">
+            <h2 class="orange">Confiram algumas empresas que apostam na gente</h2>
+        </div>
+
+        <div class="row brands mt-30">
+            <div class="col-md-4 col-xs-6"><img src="images/samsung.png" alt="Samsung"></div>
+            <div class="col-md-4 col-xs-6"><img src="images/vivara.png" alt="Vivara"></div>
+            <div class="col-md-4 col-xs-6"><img src="images/grupocomporte.png" alt="Grupo Comporte"></div>
+        </div>
+        <div class="row brands mt-30">
+            <div class="col-md-4 col-xs-6"><img src="images/brinks.png" alt="Brinks"></div>
+            <div class="col-md-4 col-xs-6"><img src="images/kumon.png" alt="Kumon"></div>
+            <div class="col-md-4 col-xs-6"><img src="images/passaro.png" alt="Pássaro Marrom"></div>
+        </div>
+    </section>
+
+    <section class="pd-60 bg-lgray">
+        <div class="container">
+            <div class="row mt-30">
+                <div class="col-md-6 pr-30">
+                    <h3 class="text-black"><span class="orange">Eleve</span> seu atendimento para <span class="orange">outro nível</span>.</h3>
+                    <p>Fale com nossos especialistas e saiba como podemos levar seu atendimento para o próximo nível.</p>
+                </div>
+                <div class="col-md-6">
+                    <form action="send.php" method="post" id="form-demonstracao" class="bg-white mtn-30">
+                        <div class="row form-group">
+                            <label class="text-black" for="text-1">Nome</label>
+                        <input id="text1" name="name" type="text" required class="form-control" placeholder="Nome">
+                        </div>
+                        <div class="row form-group">
+                            <label class="text-black" for="">Seu Melhor E-mail</label>
+                        <input id="text" name="email" type="text" required class="form-control" placeholder="Seu melhor e-mail">
+                        </div>
+                        <div class="row form-group">
+                            <label class="text-black" for="Telefone">Telefone</label>
+                        <input id="text2" name="telefone" type="text" required class="form-control" placeholder="Telefone">
+                        </div>
+                        <div class="form-group cb-inline">
+                            
+                            <label for="checkbox_0" class="control-label text-orange">
+                                <input name="checkbox" id="checkbox_0" type="checkbox" required class="custom-control-input" value="1"> 
+                                Estou de acordo com a <a href="" target="_blank">política de privacidade e termos de uso</a>
+                            </label>
+                        </div>
+                        <div class="row form-group mt-30">
+                        <button name="submit" type="submit" class="btn-out">Solicitar Demonstração</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+
+    <footer class="bg-dark">
+        <div class="container text-center">
+            <p class="text-white">© Copyright 2023 ComboCX</p>
+        </div>
+    </footer>
+</body>
+</html>
 
